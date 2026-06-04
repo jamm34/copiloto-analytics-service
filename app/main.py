6a95e45d46156e4e047c21840d90fe6f9cefabae
jamm34 <jamreyes26@gmail.com>
2026-06-03 23:47:42 -0600

    fix cors and main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.test_connection import router as test_router
 from app.api.customer_test import router as customer_router
-from app.api.analysis import (
-    router as analysis_router
-)
-from fastapi.middleware.cors import CORSMiddleware
+from app.api.analysis import router as analysis_router
 
 import app.models
 
@@ -13,40 +12,20 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(...)
-
-# Registrar rutas
 app.include_router(test_router)
 app.include_router(customer_router)
-app.include_router(
-    analysis_router
-)
+app.include_router(analysis_router)
 
 @app.get("/health")
 def health():
     return {
         "status": "ok"
     }
-
-from app.api.analysis import (
-    router as analysis_router
-)
-
-app.include_router(
-    analysis_router
-)
-
-
-
